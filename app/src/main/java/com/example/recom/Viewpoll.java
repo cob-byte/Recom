868b34bc8d19d55f716730499b5b4f980e3438ef
shnmarie <cshienalee490@gmail.com>
2023-01-10 00:02:59 +0800

    View specific poll

---
+++ app/src/main/java/com/example/recom/Viewpoll.java
@@ -0,0 +1,14 @@
+package com.example.recom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Viewpoll extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viewpoll);
+    }
+}
